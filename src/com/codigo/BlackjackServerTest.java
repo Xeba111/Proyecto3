de437757a521c38bfe51c849a104aa4dc99a9193
Xeba111 <sebasmena72@yahoo.com>
2022-03-03 17:03:41 -0500

    Funciona el servidor!

--- src/com/codigo/BlackjackServerTest.java
+++ src/com/codigo/BlackjackServerTest.java
@@ -1,9 +1,14 @@
 package com.codigo;
 
+import javax.swing.JFrame;
+
 public class BlackjackServerTest {
 
     public static void main(String[] args)
     {
-	// write your code here
+        BlackjackServer application = new BlackjackServer();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.execute();
+
     }
 }
